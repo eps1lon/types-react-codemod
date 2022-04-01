91da3fd56d5116f90475bd9161e61bd432f5ab04
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2022-04-01 20:28:16 +0200

    Scaffold transformers

---
+++ transforms/context-any.js
@@ -0,0 +1,22 @@
+const parseSync = require("./utils/parseSync");
+
+throw new Error("not implemented");
+
+/**
+ * @type {import('jscodeshift').Transform}
+ * test:
+ */
+const transformer = (file, api) => {
+	const j = api.jscodeshift;
+	const ast = parseSync(file);
+
+	const changedIdentifiers = [];
+
+	// Otherwise some files will be marked as "modified" because formatting changed
+	if (changedIdentifiers.length > 0) {
+		return ast.toSource();
+	}
+	return file.source;
+};
+
+export default transformer;
