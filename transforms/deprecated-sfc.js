94241ac2346a1e03d0163b0e6230d1ee78b21728
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2022-04-02 10:39:03 +0200

    Implement deprecated-sfc

--- transforms/deprecated-sfc.js
+++ transforms/deprecated-sfc.js
@@ -1,16 +1,20 @@
 const parseSync = require("./utils/parseSync");
 
-throw new Error("not implemented");
-
 /**
  * @type {import('jscodeshift').Transform}
- * test:
+ * test: https://astexplorer.net/#/gist/ea9d61a7e259eb28984136ba37d39922/8d0777b75f0651193991c1e05343de50b292d4aa
  */
 const transformer = (file, api) => {
 	const j = api.jscodeshift;
 	const ast = parseSync(file);
 
-	const changedIdentifiers = [];
+	const changedIdentifiers = ast
+		.find(j.Identifier, (node) => {
+			return node.name === "SFC";
+		})
+		.replaceWith(() => {
+			return j.identifier("FC");
+		});
 
 	// Otherwise some files will be marked as "modified" because formatting changed
 	if (changedIdentifiers.length > 0) {
