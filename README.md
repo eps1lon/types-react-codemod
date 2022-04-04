85b0f3bbb5ceb367a1e7296053711af11974abdf
eps1lon <silbermann.sebastian@gmail.com>
2022-04-04 14:10:38 +0200

    feat: Implement `implicit-children`

--- README.md
+++ README.md
@@ -29,9 +29,9 @@ Options:
 - `deprecated-sfc-element`
 - `deprecated-sfc`
 - `deprecated-stateless-component`
+- `implicit-children`
 - `useCallback-implicit-any`
 - `context-any` (not implemented)
-- `implicit-children` (not implemented)
 
 ## False-Positives
 
@@ -45,3 +45,19 @@ The reason being that a false-positive can be reverted easily (assuming you use 
 They simply rename identifiers with a specific name.
 If you have a type with the same name from a different package, then the rename results in a false positive.
 For example, `ink` also has a `StatelessComponent` but you don't need to rename that type since it's not deprecated.
+
+### `implicit-children`
+
+#### `implicit-children` false-positive pattern A
+
+We'll apply `React.PropsWithChildren` everytime.
+If you have a component that doesn't actually take `children`, we'll not fix what removal of implicit children should've fixed.
+
+Similarly, if your props already have `children` declared, `PropsWithChildren` will be redundant.
+Redundant `PropsWithChildren` are only problematic stylistically.
+
+#### `implicit-children` false-positive pattern B
+
+`MyFunctionComponent<Props>` where `MyFunctionComponent` comes from `import { FunctionComponent as MyFunctionComponent } from 'react'` will be ignored.
+In other words, the transform will not wrap `Props` in `React.PropsWithChildren`.
+The transform would need to implement scope tracking for this pattern to get fixed.
--- transforms/implicit-children.js
+++ transforms/implicit-children.js
@@ -1,21 +1,62 @@
 const parseSync = require("./utils/parseSync");
 
-/* eslint-disable no-unreachable */
-throw new Error("not implemented");
-
 /**
  * @type {import('jscodeshift').Transform}
- * test:
+ * test: https://astexplorer.net/#/gist/d3b8fc23045213d7ab8568048f99d786/2d1441d25eaed45c6e1d6e2a5ecf6548145c264a
  */
 const transformer = (file, api) => {
-	// eslint-disable-next-line no-unused-vars
 	const j = api.jscodeshift;
 	const ast = parseSync(file);
 
-	const changedIdentifiers = [];
+	let changedSome = false;
+
+	ast
+		.find(j.TSTypeReference, (typeReference) => {
+			const { typeName } = typeReference;
+			if (typeName.type === "TSTypeParameter") {
+				// TODO: What code produces this AST?
+				return false;
+			} else {
+				const identifier =
+					typeName.type === "TSQualifiedName"
+						? /** @type {any} */ (typeName.right)
+						: typeName;
+				return [
+					"ComponentType",
+					"FunctionComponent",
+					"FC",
+					// include deprecated types to ensure this transform works without `deprecated-*` transforms.
+					"SFC",
+					"StatelessComponent",
+				].includes(identifier.name);
+			}
+		})
+		.forEach((typeReference) => {
+			// TODO: Check if `React.PropsWithChildren` or `PropsWithChildren` is available
+			const propsWithChildrenTypeName = j.tsQualifiedName(
+				j.identifier("React"),
+				j.identifier("PropsWithChildren")
+			);
+
+			const params = typeReference.get("typeParameters").get("params")
+				.value || [j.tsTypeLiteral([])];
+			// <React.PropsWithChildren<params[0]>, ...params>
+			const paramsWithPropsWithChildren = j.tsTypeParameterInstantiation([
+				j.tsTypeReference(
+					propsWithChildrenTypeName,
+					j.tsTypeParameterInstantiation([params[0]])
+				),
+				...params.slice(1),
+			]);
+
+			// Ideally we'd clone the `typeReference` path and add `typeParameters`.
+			// But I don't know if there's an API or better pattern for it.
+			typeReference.value.typeParameters = paramsWithPropsWithChildren;
+			changedSome = true;
+		});
 
 	// Otherwise some files will be marked as "modified" because formatting changed
-	if (changedIdentifiers.length > 0) {
+	if (changedSome) {
 		return ast.toSource();
 	}
 	return file.source;
