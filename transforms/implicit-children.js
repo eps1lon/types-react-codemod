867021f4cecd8b826df06d1c1526be1c2c27fc7a
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2022-04-04 15:44:07 +0200

    Dont trigger ban-types with default options

--- transforms/implicit-children.js
+++ transforms/implicit-children.js
@@ -2,7 +2,7 @@ const parseSync = require("./utils/parseSync");
 
 /**
  * @type {import('jscodeshift').Transform}
- * test: https://astexplorer.net/#/gist/d3b8fc23045213d7ab8568048f99d786/2d1441d25eaed45c6e1d6e2a5ecf6548145c264a
+ * test: https://astexplorer.net/#/gist/d3b8fc23045213d7ab8568048f99d786/e95e6f7022ed017162d4ae593ba033d2f81958d2
  */
 const transformer = (file, api) => {
 	const j = api.jscodeshift;
@@ -39,7 +39,12 @@ const transformer = (file, api) => {
 			);
 
 			const params = typeReference.get("typeParameters").get("params")
-				.value || [j.tsTypeLiteral([])];
+				.value ?? [
+				// The idea is to produce a type that is just `{ children?: ReactNode }`
+				// Both `unknown` and `{}` would produce that since `PropsWithChildren` is implemented as `P & { children?: ReactNode }`.
+				// However, `{}` is commonly rejected by `@typescript/eslint-plugin`: https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/docs/rules/ban-types.md#default-options
+				j.tsUnknownKeyword(),
+			];
 			// <React.PropsWithChildren<params[0]>, ...params>
 			const paramsWithPropsWithChildren = j.tsTypeParameterInstantiation([
 				j.tsTypeReference(
