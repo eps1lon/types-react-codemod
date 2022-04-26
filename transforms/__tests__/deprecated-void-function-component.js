d3660ef923ca91ff527071a79bc2241559560199
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2022-04-26 21:23:18 +0200

    feat: Add `deprecated-void-function-component` transform (#52)

---
+++ transforms/__tests__/deprecated-void-function-component.js
@@ -0,0 +1,85 @@
+import { describe, expect, test } from "@jest/globals";
+import dedent from "dedent";
+import * as JscodeshiftTestUtils from "jscodeshift/dist/testUtils";
+import deprecatedVoidFunctionComponentTransform from "../deprecated-void-function-component";
+
+function applyTransform(source, options = {}) {
+	return JscodeshiftTestUtils.applyTransform(
+		deprecatedVoidFunctionComponentTransform,
+		options,
+		{
+			path: "test.d.ts",
+			source: dedent(source),
+		}
+	);
+}
+
+describe("transform deprecated-void-function-component", () => {
+	test("not modified", () => {
+		expect(
+			applyTransform(`
+				import { FC } from 'react';
+				FC;
+    `)
+		).toMatchInlineSnapshot(`
+		"import { FC } from 'react';
+						FC;"
+	`);
+	});
+
+	test("named import", () => {
+		expect(
+			applyTransform(`
+				import { VFC, VoidFunctionComponent } from 'react';
+				VFC;
+				VoidFunctionComponent;
+    `)
+		).toMatchInlineSnapshot(`
+		"import { FC, FunctionComponent } from 'react';
+						FC;
+						FunctionComponent;"
+	`);
+	});
+
+	test("named renamed import", () => {
+		expect(
+			applyTransform(`
+				import { VFC as MyVFC, VoidFunctionComponent as MyVoidFunctionComponent } from 'react';
+				MyVFC;
+				MyVoidFunctionComponent;
+    `)
+		).toMatchInlineSnapshot(`
+		"import { FC as MyVFC, FunctionComponent as MyVoidFunctionComponent } from 'react';
+						MyVFC;
+						MyVoidFunctionComponent;"
+	`);
+	});
+
+	test("namespace import", () => {
+		expect(
+			applyTransform(`
+				import * as React from 'react';
+				React.VFC;
+				React.VoidFunctionComponent;
+    `)
+		).toMatchInlineSnapshot(`
+		"import * as React from 'react';
+						React.FC;
+						React.FunctionComponent;"
+	`);
+	});
+
+	test("false-positive rename on different namespace", () => {
+		expect(
+			applyTransform(`
+				import * as Preact from 'preact';
+				Preact.VFC;
+				Preact.VoidFunctionComponent;
+    `)
+		).toMatchInlineSnapshot(`
+		"import * as Preact from 'preact';
+						Preact.FC;
+						Preact.FunctionComponent;"
+	`);
+	});
+});
