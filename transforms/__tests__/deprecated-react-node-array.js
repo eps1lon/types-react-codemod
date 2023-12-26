ee5e7e08f963b8f354860799f469759ef26e5cce
Sebastian Silbermann <sebastian.silbermann@klarna.com>
2023-12-26 12:12:40 +0100

    Improve coverage

--- transforms/__tests__/deprecated-react-node-array.js
+++ transforms/__tests__/deprecated-react-node-array.js
@@ -34,6 +34,22 @@ describe("transform deprecated-react-node-array", () => {
 	test("named import", () => {
 		expect(
 			applyTransform(`
+      import { ReactNodeArray } from 'react';
+		interface Props {
+				children?: ReactNodeArray;
+			}
+    `),
+		).toMatchInlineSnapshot(`
+		"import { ReactNode } from 'react';
+		interface Props {
+				children?: ReadonlyArray<ReactNode>;
+			}"
+	`);
+	});
+
+	test("named import with existing ReactNode import", () => {
+		expect(
+			applyTransform(`
       import { ReactNodeArray, ReactNode } from 'react';
 		interface Props {
 				children?: ReactNodeArray;
