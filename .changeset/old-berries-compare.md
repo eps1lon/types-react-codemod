ce68c5693c8028e2449132f8b7223fcdcc379b7e
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2024-03-18 20:09:24 +0100

    fix: Include `deprecated-prop-types-types` transform in `preset-19` (#365)

---
+++ .changeset/old-berries-compare.md
@@ -0,0 +1,5 @@
+---
+"types-react-codemod": patch
+---
+
+Include `deprecated-prop-types-types` transform in `preset-19`
--- bin/types-react-codemod.cjs
+++ bin/types-react-codemod.cjs
@@ -99,6 +99,7 @@ async function main() {
 							type: "checkbox",
 							choices: [
 								{ checked: true, value: "deprecated-legacy-ref" },
+								{ checked: true, value: "deprecated-prop-types-types" },
 								{ checked: true, value: "deprecated-react-child" },
 								{ checked: true, value: "deprecated-react-node-array" },
 								{ checked: true, value: "deprecated-react-fragment" },
